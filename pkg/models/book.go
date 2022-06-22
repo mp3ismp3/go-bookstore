c2b8b78e2976259c9133298e4c65c870a097cf36
mp3ismp3 <peter2mary@gmail.com>
2022-06-22 18:52:03 +0800

    init commit

---
+++ pkg/models/book.go
@@ -0,0 +1,45 @@
+package models
+
+import (
+	"github.com/jinzhu/gorm"
+	"github.com/mp3ismp3/go-bookstore/pkg/config"
+)
+
+var db *gorm.DB
+
+type Book struct {
+	gorm.Model
+	Name        string `gorm:""json:"name"`
+	Author      string `json:"author"`
+	Publication string `json:"publication"`
+}
+
+func init() {
+	config.Connect()
+	db = config.GetDB()
+	db.AutoMigrate(&Book{}) //其作用主要是刷新数据库中的表格，使其保持最新，即让数据库之前存储的记录的表格字段和程序中最新使用的表格字段保持一致（只增不减）
+}
+
+func (b *Book) CreateBook() *Book {
+	db.NewRecord(b)
+	db.Create(&b)
+	return b
+}
+
+func GetAllBooks() []Book {
+	var Books []Book
+	db.Find(&Books)
+	return Books
+}
+
+func GetBookById(Id int64) (*Book, *gorm.DB) {
+	var getBook Book
+	db := db.Where("ID=?", Id).Find(&getBook)
+	return &getBook, db
+}
+
+func DeleteBook(ID int64) Book {
+	var book Book
+	db.Where("ID=?", ID).Delete(book)
+	return book
+}
